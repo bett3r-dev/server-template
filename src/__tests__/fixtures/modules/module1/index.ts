da6893a1b55056782b97f6b97352d47d20dbc227
Tomas Ruiz <tomasruizr@gmail.com>
2023-01-25 07:37:00 -0300

    chore: code cleanup

--- src/__tests__/fixtures/modules/module1/index.ts
+++ src/__tests__/fixtures/modules/module1/index.ts
@@ -3,7 +3,4 @@ import { Libraries } from "src";
 export function create(libraries: Libraries) {
   expect((libraries.library2 as any).helloUpperCase).toBeDefined();
   expect((libraries.library2 as any).helloUpperCase('tomas')).toBe('Hello TOMAS');
-  libraries.serverInitEmitter.on('serverReady', () => {
-    console.log('asdf');
-  })
 }
